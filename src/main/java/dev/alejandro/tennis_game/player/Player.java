0b2793ace06a9543089689abb50d21038af9cf7e
AlejandroAriasSL <alejandroasilva02@gmail>
2025-05-21 19:15:41 +0200

    Feature: add Player entity with JPA annotations

---
+++ src/main/java/dev/alejandro/tennis_game/player/Player.java
@@ -0,0 +1,31 @@
+package dev.alejandro.tennis_game.player;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="players")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "player_id", nullable = false, unique = true)
+    private Long id; 
+    
+    @Column(name = "player_name", nullable = false)
+    private String name;
+
+    public Player(String name, Long id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() { return name; }
+    public Long getId() { return id; }
+
+    public void setName(String name) { this.name = name; }
+}
