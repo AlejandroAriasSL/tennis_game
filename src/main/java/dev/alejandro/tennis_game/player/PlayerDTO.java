1539479477afc37d041816b52830c9c8955274ce
AlejandroAriasSL <alejandroasilva02@gmail>
2025-05-21 20:18:52 +0200

    Feature: PlayerDTO is able to be build statically from an entity, green

--- src/main/java/dev/alejandro/tennis_game/player/PlayerDTO.java
+++ src/main/java/dev/alejandro/tennis_game/player/PlayerDTO.java
@@ -2,6 +2,10 @@ package dev.alejandro.tennis_game.player;
 
 public record PlayerDTO(String name, Long id) {
 
+    public static PlayerDTO fromEntity(Player player){
+        return new PlayerDTO(player.getName(), player.getId());
+    } 
+
     public Player toEntity(){
         return new Player(this.name, this.id);
     }
