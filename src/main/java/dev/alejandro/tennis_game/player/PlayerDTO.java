e85b047c9ba80bea669063b16e95d68e34aa392f
AlejandroAriasSL <alejandroasilva02@gmail>
2025-05-21 20:11:59 +0200

    Feature: PlayerDTO creates Player entity correctly, red

--- src/main/java/dev/alejandro/tennis_game/player/PlayerDTO.java
+++ src/main/java/dev/alejandro/tennis_game/player/PlayerDTO.java
@@ -1,5 +1,8 @@
 package dev.alejandro.tennis_game.player;
 
 public record PlayerDTO(String name, Long id) {
-    
+
+    public Player toEntity(){
+        return new Player(this.name, this.id);
+    }
 }
