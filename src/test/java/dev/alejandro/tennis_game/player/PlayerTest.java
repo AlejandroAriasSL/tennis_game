5e70958d7e0d4b6fedee7e9918a3c9fb1cef1425
AlejandroAriasSL <alejandroasilva02@gmail>
2025-05-21 19:34:22 +0200

    Refactor: refactor instance initialization with beforeEach

--- src/test/java/dev/alejandro/tennis_game/player/PlayerTest.java
+++ src/test/java/dev/alejandro/tennis_game/player/PlayerTest.java
@@ -3,23 +3,29 @@ package dev.alejandro.tennis_game.player;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.isA;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 public class PlayerTest {
 
+    String name;
+    Long id;
+    Player player;
+
+    @BeforeEach
+    void setUp(){
+        name = "Player1";
+        id = 1L;
+        player = new Player(name, id); 
+    }
+
 
     @Test
     @DisplayName("Player is created with correct attributes")
     void test_player_attributes(){
-
-        String name = "player1";
-        Long id = 1L;
-
-        Player player = new Player(name, id);
 
         assertThat(player.getName(), is(equalTo(name)));
         assertThat(player.getId(), is(equalTo(id)));
@@ -29,10 +35,6 @@ public class PlayerTest {
     @DisplayName("Player can change name")
     void test_player_name_change(){
 
-        String name = "player1";
-        Long id = 1L;
-
-        Player player = new Player(name, id);
         String newName = "player2";
         
         player.setName(newName);
@@ -44,12 +46,8 @@ public class PlayerTest {
     @DisplayName("Player starts with 0 points")
     void test_player_initial_points_are_zero(){
 
-        String name = "Player1";
-        Long id = 1L;
-
-        Player player = new Player(name, id);
         int points = player.getPoints();
 
-        assertThat(player.getPoints(), is(equalTo(0)));
+        assertThat(points, is(equalTo(0)));
     }
 }
