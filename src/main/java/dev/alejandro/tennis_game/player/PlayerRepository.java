28f8c776838ea5894cbe5b7da404da69376ea79b
AlejandroAriasSL <alejandroasilva02@gmail>
2025-05-21 20:33:07 +0200

    Feature: add PlayerRepository with JPA

---
+++ src/main/java/dev/alejandro/tennis_game/player/PlayerRepository.java
@@ -0,0 +1,9 @@
+package dev.alejandro.tennis_game.player;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long>{
+
+}
