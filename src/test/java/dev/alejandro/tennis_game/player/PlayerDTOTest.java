c190c79de7229d5f57c05265eda438893185731d
AlejandroAriasSL <alejandroasilva02@gmail>
2025-05-21 20:10:49 +0200

    Test: PlayerDTO creates Player correctly, red

--- src/test/java/dev/alejandro/tennis_game/player/PlayerDTOTest.java
+++ src/test/java/dev/alejandro/tennis_game/player/PlayerDTOTest.java
@@ -3,6 +3,7 @@ package dev.alejandro.tennis_game.player;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.isA;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +21,21 @@ public class PlayerDTOTest {
 
         assertThat(playerDto.name(), is(equalTo(name)));
         assertThat(playerDto.id(), is(equalTo(id)));
+    }
+
+    @Test
+    @DisplayName("PlayerDTO creates Player correctly")
+    void test_playerDTO_creates_player_instance(){
+
+        String name = "Player1";
+        Long id = 1L;
+
+        PlayerDTO playerDto = new PlayerDTO(name, id);
+        Player player = playerDto.toEntity();
+
+        assertThat(player.getClass(), is(equalTo(Player.class)));
+        assertThat(player.getName(), is(equalTo(name)));
+        assertThat(player.getId(), is(equalTo(id)));
     }
 
 }
