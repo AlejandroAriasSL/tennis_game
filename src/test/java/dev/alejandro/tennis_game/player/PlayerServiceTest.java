63fc258763b90feef80705c2e2476906a71c47d8
AlejandroAriasSL <alejandroasilva02@gmail>
2025-05-21 21:19:10 +0200

    Refactor: refactor PlayerServiceTest initial mock of PlayerRepository, initialization of PlayerService, default attributes and BeforeEach annotation

--- src/test/java/dev/alejandro/tennis_game/player/PlayerServiceTest.java
+++ src/test/java/dev/alejandro/tennis_game/player/PlayerServiceTest.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,16 +17,24 @@ import jakarta.persistence.EntityNotFoundException;
 
 public class PlayerServiceTest {
 
+    String name;
+    Long id;
+    PlayerRepository playerRepository;
+    PlayerService playerService;
+
+    @BeforeEach
+    void setUp(){
+        name = "Player1";
+        id = 1L;
+        playerRepository = mock(PlayerRepository.class);
+        playerService = new PlayerService(playerRepository);
+    }
+
 
     @Test
     @DisplayName("PlayerService returns player by ID when found")
     void test_player_found_by_id(){
 
-        PlayerRepository playerRepository = mock(PlayerRepository.class);
-        PlayerService playerService = new PlayerService(playerRepository);
-
-        String name = "Player1";
-        Long id = 1L;
         Player player = new Player(name, id);
 
         when(playerRepository.findById(id)).thenReturn(Optional.of(player));
@@ -39,11 +48,6 @@ public class PlayerServiceTest {
     @Test
     @DisplayName("PlayerService throws exception when player not found by ID")
     void test_player_not_found_throws_exception(){
-
-        PlayerRepository playerRepository = mock(PlayerRepository.class);
-        PlayerService playerService = new PlayerService(playerRepository);
-
-        Long id = 1L;
 
         when(playerRepository.findById(id)).thenReturn(Optional.empty());
 
