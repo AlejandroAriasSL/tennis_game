720622edea31f077fc5d38d8f5bc95ca305537ff
AlejandroAriasSL <alejandroasilva02@gmail>
2025-05-22 12:49:02 +0200

    Test: PlayerController getAllPlayers returns list of playerDTOs

--- src/test/java/dev/alejandro/tennis_game/player/PlayerControllerTest.java
+++ src/test/java/dev/alejandro/tennis_game/player/PlayerControllerTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
@@ -28,5 +29,28 @@ public class PlayerControllerTest {
         assertThat(response.getStatusCode().is2xxSuccessful(), is(equalTo(true)));
         assertThat(response.getBody(), is(notNullValue()));
         assertThat(response.getBody().size(), is(0));
+    }
+
+    @Test
+    @DisplayName("PlayerController getAllPlayers returns list of playerDTOs")
+    void test_playerController_returns_list_of_playerDto(){
+
+        PlayerRepository repository = mock(PlayerRepository.class);
+        PlayerService playerService = new PlayerService(repository);
+        PlayerController playerController = new PlayerController(playerService);
+
+        List<Player> playerEntities = List.of(new Player("player1", 1L), new Player("player2", 2L));
+
+        when(repository.findAll()).thenReturn(playerEntities);
+
+        ResponseEntity<List<PlayerDTO>> response = playerController.getAllPlayers();
+
+        List<PlayerDTO> playerDtos = playerEntities.stream().map(PlayerDTO::fromEntity).toList(); 
+
+        assertThat(response.getStatusCode().is2xxSuccessful(), is(equalTo(true)));
+        assertThat(response.getBody(), is(notNullValue()));
+        assertThat(response.getBody().size(), is(2));
+        assertThat(response.getBody(), is(equalTo(playerDtos)));
+
     }
 }
