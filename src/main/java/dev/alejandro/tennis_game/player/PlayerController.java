703f05c4ae940485912bf4ed1f736f88c09454b5
AlejandroAriasSL <alejandroasilva02@gmail>
2025-05-22 14:47:32 +0200

    Feature: add PlayerController createPlayer POST endpoint

--- src/main/java/dev/alejandro/tennis_game/player/PlayerController.java
+++ src/main/java/dev/alejandro/tennis_game/player/PlayerController.java
@@ -3,8 +3,11 @@ package dev.alejandro.tennis_game.player;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +30,12 @@ public class PlayerController {
     public ResponseEntity<PlayerDTO> getPlayerById(@PathVariable final Long id){
         return ResponseEntity.ok(playerService.getPlayerById(id));
     }
+
+    @PostMapping
+    public ResponseEntity<String> createPlayer(@RequestBody final PlayerDTO playerDto){
+        playerService.createOrUpdate(playerDto);
+        return ResponseEntity.ok("Jugador creado con éxito");
+    }
+
+    
 }
