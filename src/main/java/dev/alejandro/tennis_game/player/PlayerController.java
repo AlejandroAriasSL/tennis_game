753eaf68261910d0bba5c07dc918554e9a9fc957
AlejandroAriasSL <alejandroasilva02@gmail>
2025-05-22 13:17:24 +0200

    Feature: add PlayerController getPlayerById GET endpoint

--- src/main/java/dev/alejandro/tennis_game/player/PlayerController.java
+++ src/main/java/dev/alejandro/tennis_game/player/PlayerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class PlayerController {
     @GetMapping
     public ResponseEntity<List<PlayerDTO>> getAllPlayers(){
         return ResponseEntity.ok(playerService.getAllPlayers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PlayerDTO> getPlayerById(@PathVariable final Long id){
+        return ResponseEntity.ok(playerService.getPlayerById(id));
     }
 }
