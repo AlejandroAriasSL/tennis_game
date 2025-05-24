b29a59ee31b8587684fb19079a4a0a7fce1bf92e
AlejandroAriasSL <alejandroasilva02@gmail>
2025-05-24 17:24:48 +0200

    Configuration: add AbstractIntegrationTest setup with MySQL docker image

---
+++ src/test/java/dev/alejandro/tennis_game/player/AbstractIntregrationTest.java
@@ -0,0 +1,38 @@
+package dev.alejandro.tennis_game.player;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import org.testcontainers.containers.MySQLContainer;
+
+import dev.alejandro.tennis_game.TennisGameApplication;
+
+@SpringBootTest(
+    classes = TennisGameApplication.class,
+    webEnvironment = WebEnvironment.RANDOM_PORT
+)
+@ActiveProfiles("test")
+abstract class AbstractIntegrationTest {
+
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.4.4")
+        .withDatabaseName("tennisgame")
+        .withUsername("testuser")
+        .withPassword("testpass"); 
+
+    static {
+        mysql.start();
+    }
+
+    @DynamicPropertySource
+    static void mySQLProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> 
+        String.format("jdbc:mysql://%s:%d/tennisgame?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
+                      mysql.getHost(), mysql.getFirstMappedPort()));
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
+
+}
