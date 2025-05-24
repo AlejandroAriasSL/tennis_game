c0b5dd4e1ea196612eb2be382a6dbf31674376b3
AlejandroAriasSL <alejandroasilva02@gmail>
2025-05-24 17:41:43 +0200

    Test: It should return a list of players and 2xx response

--- src/test/java/dev/alejandro/tennis_game/player/PlayerControllerIntegrationTest.java
+++ src/test/java/dev/alejandro/tennis_game/player/PlayerControllerIntegrationTest.java
@@ -3,6 +3,9 @@ package dev.alejandro.tennis_game.player;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +14,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 
 import dev.alejandro.tennis_game.player.requests.CreatePlayerRequest;
+import dev.alejandro.tennis_game.player.requests.UpdatePlayerRequest;
 
 
 public class PlayerControllerIntegrationTest extends AbstractIntegrationTest {
@@ -28,5 +32,22 @@ public class PlayerControllerIntegrationTest extends AbstractIntegrationTest {
         assertThat(postResponse.getStatusCode().is2xxSuccessful()).isTrue();
         assertThat(postResponse.getBody()).isNotNull();
         assertThat(postResponse.getBody().contains("Jugador creado con éxito"));
+    }
+
+    @Test
+    @DisplayName("It should return a list of players and 2xx response")
+    void test_create_and_get_player_returns_2XX(){
+        CreatePlayerRequest request = new CreatePlayerRequest("Player1");
+        restTemplate.postForEntity("/api/player", request, Void.class);
+
+        ResponseEntity<UpdatePlayerRequest[]> getResponse = restTemplate.getForEntity("/api/player", UpdatePlayerRequest[].class);
+
+        List<String> playerNames = Arrays.stream(getResponse.getBody())
+                                         .map(UpdatePlayerRequest::name)
+                                         .toList();
+
+        assertThat(getResponse.getStatusCode().is2xxSuccessful()).isTrue();
+        assertThat(getResponse.getBody()).isNotNull();
+        assertThat(playerNames).contains(request.name());
     }
 }
